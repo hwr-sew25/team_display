dcf5a71fd2652bc04bb80574fd688a6e40571dbe
Julia Kantor <julia.kantor4@gmail.com>
2025-11-21 14:05:11 +0100

    add translation

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -16,6 +16,12 @@ def index():
 @app.route("/raumwahl")
 def raumwahl():
     return render_template("raumwahl.html")
+
+@app.route("/set_language/<lang>")
+def set_language(lang):
+    session['lang'] = lang
+    # zurück zur vorherigen Seite
+    return redirect(request.referrer or url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
