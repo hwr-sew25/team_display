7f700c5cc312d0a57df480f18f5da546e649665c
Julia Kantor <julia.kantor4@gmail.com>
2025-12-12 11:55:27 +0100

    add active_screen sessions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, session, request, redirect, url_for
+import roslibpy
 
 app = Flask(__name__)
 
@@ -7,18 +8,29 @@ app.config.from_mapping(
     BOOTSTRAP_BOOTSWATCH_THEME = 'pulse'
 )
 
+#client = roslibpy.Ros(host='10.20.128.225', port=9090)
+#client.run()
+
+#screen_pub = roslibpy.Topic(client, '/current_screen', 'std_msgs/String')
+
+#def publish_screen(screen_name):
+#    screen_pub.publish(roslibpy.Message({'data': screen_name}))
+
 
 @app.route('/')
 @app.route('/index')
 def index():
+    session["active_screen"] = "start"
     return render_template('index.html')
 
 @app.route("/raumwahl")
 def raumwahl():
+    session["active_screen"] = "raumwahl"
     return render_template("raumwahl.html")
 
 @app.route("/karte")
 def karte():
+    session["active_screen"] = "karte"
     return render_template("karte.html", direction="left")
 
 @app.route("/set_language/<lang>")
@@ -29,10 +41,12 @@ def set_language(lang):
 
 @app.route("/error1")
 def error_route():
+    session["active_screen"] = "error1"
     return render_template("error_route.html")
 
 @app.route("/error2")
 def error_tec():
+    session["active_screen"] = "error2"
     return render_template("error_tec.html")
 
 if __name__ == "__main__":
